71aded14e5b9b04630b55ef7bd03a52b2d1b5a29
Arobase <sergedelonboka92@gmail.com>
2022-09-12 19:07:33 -0700

    Change the variable last_digit to digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,19 +13,19 @@
 
 int main(void)
 {
-	int n, last_digit;
+	int n, digit;
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	digit = n % 10;
 
-	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	if (last_digit < 6 && last_digit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-	if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	if (digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	if (digit < 6 && digit != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	if (digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 
 	return (0);
 }
