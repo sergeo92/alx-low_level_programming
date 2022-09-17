71e9eca1b0e9ddf53fd839d195113e89bccd4235
Arobase <sergedelonboka92@gmail.com>
2022-09-17 16:30:27 -0700

    Print all combinaison of three digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of three digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			for (k = j + 1; k > 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i < 55 || j < 56 || k < 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+
+	putchar(10);
+	return (0);
+}
