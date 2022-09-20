a0c0e0730cf7dc5a144dfb624d1a52f3a9b1d596
Arobase <sergedelonboka92@gmail.com>
2022-09-19 19:20:03 -0700

    Add the documentation to function in the file 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value it points to to 98
+ * reset_to_98 - updates the value it points to 98
  *
- * @*n: Address of variable to update
+ * Desccription: Update the value it points to 98
+ *
+ * @n: Address of variable to update
  *
  */
 void reset_to_98(int *n)
