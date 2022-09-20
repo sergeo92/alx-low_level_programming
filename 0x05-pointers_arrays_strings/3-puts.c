2d95d4657993f0f4ec3099dfce4c705987ca85a1
Arobase <sergedelonboka92@gmail.com>
2022-09-19 20:05:08 -0700

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string
+ *
+ * @str: String to print
+ *
+ * Description: Print a string
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
